d606a3dbf5ce8e58ef09f28dee58de86cac1c53a
Peter van der Heijden <contact@peterjarian.dev>
2025-06-05 14:19:15 +0200

    Initial commit

--- scripts/prepare-kernel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing dependencies..."
-poetry install
-
-echo "Installing Jupyter kernel..."
-poetry run python -m ipykernel install --user --name=myenv --display-name "Python (myenv)"
-
-echo "Done preparing Dev Container, ready for use."
